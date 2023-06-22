744adffcbdf2792d622a87c84f22b541d162b574
Marion Jung <marionjung34@hotmail.com>
2023-06-22 17:46:30 +0100

    Additional tests

--- spec/printStatement.spec.js
+++ spec/printStatement.spec.js
@@ -2,7 +2,7 @@ import Statement from "../src/Statement.js";
 
 describe('Testing the Statement class:', () => {
 
-    it('Test 5: the printStatement static method should have been called once ', () => {
+    it('Test 10: the printStatement static method should have been called once', () => {
         // Arrange
         const printingSpy = spyOn(Statement, `printStatement`);
 
@@ -11,5 +11,17 @@ describe('Testing the Statement class:', () => {
 
         // Assert
         expect(printingSpy).toHaveBeenCalledTimes(1);
+    });
+
+
+    it('Test 11: the printStatement static method should log the statement (testing the header) to the console', () => {
+        // Arrange
+        const printingSpy = spyOn(global.console, `log`);
+
+        // Act
+        Statement.printStatement();
+
+        // Assert
+        expect(printingSpy).toHaveBeenCalledWith('date       || credit  || debit  || balance');
     });
 });
--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -8,7 +8,7 @@ describe('Testing the transaction class', () => {
         const debit = 400;
         const credit = 0;
         const transaction1 = new Transaction(date, credit, debit);
-        const debitSpy = spyOn(transaction1, `getDebitAmount`)
+        const debitSpy = spyOn(transaction1, `getDebitAmount`);
 
         // Act
         transaction1.getDebitAmount();
@@ -18,13 +18,29 @@ describe('Testing the transaction class', () => {
     });
 
 
-    it('Test 5: testing that the getDebitAmount method returns the right amount: 20', () => {
+    it('Test 5: testing that the getCreditAmount method is triggered when called in the Transaction class', () => {
+        // Arrange
+        const date = "04/07/2023";
+        const debit = 0;
+        const credit = 200;
+        const transaction1 = new Transaction(date, credit, debit);
+        const debitSpy = spyOn(transaction1, `getCreditAmount`);
+
+        // Act
+        transaction1.getCreditAmount();
+
+        // Assert
+        expect(debitSpy).toHaveBeenCalledTimes(1);
+    });
+
+
+    it('Test 6: testing that the getDebitAmount method returns the right amount: 20', () => {
         // Arrange
         const date = "04/07/2023";
         const credit = 400;
         const debit = 20;
         const transaction1 = new Transaction(date, credit, debit);
-        const expected = 20
+        const expected = 20;
 
         // Act
         const actual = transaction1.getDebitAmount();
@@ -34,7 +50,23 @@ describe('Testing the transaction class', () => {
     });
 
 
-    it('Test 6: testing that the date getter returns a string date', () => {
+    it('Test 7: testing that the getCreditAmount method returns the right amount: 200', () => {
+        // Arrange
+        const date = "04/07/2023";
+        const credit = 200;
+        const debit = 0;
+        const transaction1 = new Transaction(date, credit, debit);
+        const expected = 200;
+
+        // Act
+        const actual = transaction1.getCreditAmount();
+
+        // Assert
+        expect(actual).toEqual(expected);
+    });
+
+
+    it('Test 8: testing that the date getter returns a string date', () => {
         // Arrange
         const date = "04/07/2023";
         const debit = 400;
@@ -43,5 +75,21 @@ describe('Testing the transaction class', () => {
 
         // Act & Assert
         expect(transaction2.getDate()).toBeInstanceOf(String);
+    });
+
+
+    it('Test 9: testing that the date getter returns the right value: "04/07"2023"', () => {
+        // Arrange
+        const date = "04/07/2023";
+        const credit = 200;
+        const debit = 0;
+        const transaction1 = new Transaction(date, credit, debit);
+        const expected = "04/07/2023";
+
+        // Act
+        const actual = transaction1.getDate();
+
+        // Assert
+        expect(actual).toEqual(expected);
     });
 });
