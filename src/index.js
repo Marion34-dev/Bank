7e18e6303cebaffbb77c0cdb0b4d24f1cb3b60a5
Marion Jung <marionjung34@hotmail.com>
2023-06-22 10:22:28 +0100

    Imported Chalk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Account from './Account.js';
 import Transaction from './Transaction.js';
+import Statement from './Statement.js';
 
 const account = new Account();
 
-const transaction1 = ["10/01/2012", 1000.00, 0];
-const transaction2 = ["13/01/2012", 2000.00, 0];
-const transaction3 = ["14/01/2012", 500.00, 0];
+const transaction1 = new Transaction(["10/01/2012", 1000.00, 0]);
+const transaction2 = new Transaction(["13/01/2012", 2000.00, 0]);
+const transaction3 = new Transaction(["14/01/2012", 500.00, 0]);
+
 account.deposit(transaction1);
 account.deposit(transaction2);
 account.withdraw(transaction3);
