04ff7150740bee1b335b6cea0b24a2869328d933
Marion Jung <marionjung34@hotmail.com>
2023-06-22 17:22:21 +0100

    Adding additional tests

--- spec/account.spec.js
+++ spec/account.spec.js
@@ -29,7 +29,6 @@ describe('Testing the Account class', () => {
 
     it('Test 1: it should add 300 to the balance of the account', () => {
         // Arrange
-
         const MockTransaction1 = new MockTransaction("04/07/1990", 300, 0);
         const account1 = new Account();
 
@@ -40,16 +39,33 @@ describe('Testing the Account class', () => {
         expect(account1.getBalance()).toEqual(300);
     });
 
-    it('Test 2: it should add 1 to the length of the transactions array', () => {
+
+    it('Test 2: it should remove 50 to the balance of the account', () => {
         // Arrange
+        const MockTransaction1 = new MockTransaction("04/07/1990", 300, 0);
+        const MockTransaction3 = new MockTransaction("04/07/1990", 0, 50);
+        const account1 = new Account();
+
+        // Act
+        account1.deposit(MockTransaction1);
+        account1.withdraw(MockTransaction3);
+
+        // Assert
+        expect(account1.getBalance()).toEqual(250);
+    });
+
+
+    it('Test 3: it should add 2 to the length of the transactions array (one credit, one debit)', () => {
+        // Arrange
+        const MockTransaction1 = new MockTransaction("04/07/1990", 300, 0);
         const MockTransaction2 = new MockTransaction("04/07/1990", 0, 40)
         const account2 = new Account();
 
         // Act
+        account2.deposit(MockTransaction1);
         account2.withdraw(MockTransaction2);
 
         // Assert
-        expect(account2.getTransactions().length).toBe(1);
-
+        expect(account2.getTransactions().length).toBe(2);
     })
 })
--- spec/transaction.spec.js
+++ spec/transaction.spec.js
@@ -2,12 +2,12 @@ import Transaction from '../src/Transaction.js';
 
 describe('Testing the transaction class', () => {
 
-    it('Test 3: testing that the getDebitAmount method is triggered when using it', () => {
+    it('Test 4: testing that the getDebitAmount method is triggered when called in the Transaction class', () => {
         // Arrange
         const date = "04/07/2023";
         const debit = 400;
         const credit = 0;
-        const transaction1 = new Transaction(date, debit, credit);
+        const transaction1 = new Transaction(date, credit, debit);
         const debitSpy = spyOn(transaction1, `getDebitAmount`)
 
         // Act
@@ -17,7 +17,24 @@ describe('Testing the transaction class', () => {
         expect(debitSpy).toHaveBeenCalled();
     });
 
-    it('Test 4: testing that the date getter returns a string date', () => {
+
+    it('Test 5: testing that the getDebitAmount method returns the right amount: 20', () => {
+        // Arrange
+        const date = "04/07/2023";
+        const credit = 400;
+        const debit = 20;
+        const transaction1 = new Transaction(date, credit, debit);
+        const expected = 20
+
+        // Act
+        const actual = transaction1.getDebitAmount();
+
+        // Assert
+        expect(actual).toEqual(expected);
+    });
+
+
+    it('Test 6: testing that the date getter returns a string date', () => {
         // Arrange
         const date = "04/07/2023";
         const debit = 400;
