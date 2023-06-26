6cefb2c086e8dfa8fa56206af11d7cca9ec87c8d
Marion Jung <marionjung34@hotmail.com>
2023-06-26 18:15:28 +0100

    Added unit test for robustness

--- spec/printStatement.spec.js
+++ spec/printStatement.spec.js
@@ -1,5 +1,55 @@
 import Statement from "../src/Statement.js";
 
+class MockAccount {
+    #balance;
+    #transactions;
+
+    constructor(initialBalance = 0, transaction = []) {
+        this.#balance = initialBalance;
+        this.#transactions = transaction;
+    };
+
+    getTransactions() {
+        return this.#transactions;
+    }
+
+    deposit(transactions) {
+        const date = transactions.getDate();
+        const credit = transactions.getCreditAmount();
+        const debit = transactions.getDebitAmount();
+        this.#balance += credit;
+        const currentTrans = [date, credit, debit, this.#balance];
+        return this.#transactions.push(currentTrans);
+    };
+};
+
+class MockTransaction {
+    date;
+    credit;
+    debit;
+
+    constructor(date = "", credit = 0, debit = 0) {
+        this.date = date;
+        this.credit = credit;
+        this.debit = debit;
+    }
+
+    getDate() {
+        return this.date;
+    };
+
+    getDebitAmount() {
+        return this.debit;
+    };
+
+    getCreditAmount() {
+        return this.credit;
+    };
+}
+
+const MockTransaction1 = new MockTransaction("04/07/1990", 300, 0);
+const account1 = new MockAccount();
+
 describe('Testing the Statement class:', () => {
 
     it('Test 10: the printStatement static method should have been called once', () => {
@@ -23,5 +73,17 @@ describe('Testing the Statement class:', () => {
 
         // Assert
         expect(printingSpy).toHaveBeenCalledWith('date       || credit  || debit  || balance');
+    });
+
+    it('Test 12: the printStatement static method should log the deposit details', () => {
+        // Arrange
+        const printingSpy = spyOn(Statement, `printStatement`);
+
+        // Act
+        account1.deposit(MockTransaction1);
+        Statement.printStatement(account1.getTransactions());
+
+        // Assert
+        expect(printingSpy).toHaveBeenCalledWith([['04/07/1990', 300, 0, 300]]);
     });
 });
