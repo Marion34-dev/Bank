20d6b570faea74093989eb64881929162e50d7f4
Marion Jung <marionjung34@hotmail.com>
2023-06-28 15:07:20 +0100

    Refactored Statement

--- src/Statement.js
+++ src/Statement.js
@@ -1,25 +1,13 @@
-import chalk from 'chalk';
-
 class Statement {
-
     static printStatement(transactions = []) {
         console.log("date       || credit  || debit  || balance");
 
-        transactions.reverse().forEach(trans => {
-            let date = trans[0];
-            let credit = trans[1];
-            let debit = trans[2];
-            let balance = trans[3];
+        transactions.reverse().forEach(([date, credit, debit, balance]) => {
+            const formattedCredit = credit !== 0 ? '\x1b[32m' + credit.toFixed(2) + '\x1b[0m' : '       ';
+            const formattedDebit = debit !== 0 ? '\x1b[31m' + debit.toFixed(2) + '\x1b[0m' : '      ';
+            const formattedBalance = balance >= 0 ? '\x1b[32m' + balance.toFixed(2) + '\x1b[0m' : '\x1b[31m' + balance.toFixed(2) + '\x1b[0m';
 
-            if (credit != 0) {
-                const formattedCredit = chalk.green(credit.toFixed(2));
-                const formattedBalance = balance >= 0 ? chalk.green(balance.toFixed(2)) : chalk.red(balance.toFixed(2));
-                console.log(`${date} || ${formattedCredit} ||        || ${formattedBalance}`);
-            } else if (debit != 0) {
-                const formattedDebit = chalk.red(debit.toFixed(2));
-                const formattedBalance = balance >= 0 ? chalk.green(balance.toFixed(2)) : chalk.red(balance.toFixed(2));
-                console.log(`${date} ||         || ${formattedDebit} || ${formattedBalance}`);
-            }
+            console.log(`${date} || ${formattedCredit} || ${formattedDebit} || ${formattedBalance}`);
         });
     }
 }
