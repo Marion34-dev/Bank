646c4754452ab1018e5a536b68d0c1b21d761ba5
Marion Jung <marionjung34@hotmail.com>
2023-06-28 16:05:17 +0100

    Refactored printStatement

--- spec/printStatement.spec.js
+++ spec/printStatement.spec.js
@@ -13,10 +13,15 @@ class MockAccount {
         return this.#transactions;
     }
 
+    withdraw(transactions) {
+        const { date = transactions.getDate(), credit = transactions.getCreditAmount(), debit = transactions.getDebitAmount() } = transactions;
+        this.#balance -= debit;
+        const currentTrans = [date, credit, debit, this.#balance];
+        return this.#transactions.push(currentTrans);
+    };
+
     deposit(transactions) {
-        const date = transactions.getDate();
-        const credit = transactions.getCreditAmount();
-        const debit = transactions.getDebitAmount();
+        const { date = transactions.getDate(), credit = transactions.getCreditAmount(), debit = transactions.getDebitAmount() } = transactions;
         this.#balance += credit;
         const currentTrans = [date, credit, debit, this.#balance];
         return this.#transactions.push(currentTrans);
@@ -47,14 +52,31 @@ class MockTransaction {
     };
 }
 
-const MockTransaction1 = new MockTransaction("04/07/1990", 300, 0);
-const account1 = new MockAccount();
+let expected;
+let printingSpy;
+let account1;
+let MockTransaction1;
+let MockTransaction2;
 
 describe('Testing the Statement class:', () => {
+    beforeEach(() => {
+        MockTransaction1 = new MockTransaction("04/07/1990", 300, 0);
+        MockTransaction2 = new MockTransaction("04/07/2023", 0, 50);
+        account1 = new MockAccount();
+    });
+
+    afterEach(() => {
+        expected = undefined;
+        printingSpy = undefined;
+        account1 = undefined;
+        MockTransaction1 = undefined;
+        MockTransaction2 = undefined;
+    });
+
 
     it('Test 10: the printStatement static method should have been called once', () => {
         // Arrange
-        const printingSpy = spyOn(Statement, `printStatement`);
+        printingSpy = spyOn(Statement, `printStatement`);
 
         // Act
         Statement.printStatement();
@@ -66,7 +88,7 @@ describe('Testing the Statement class:', () => {
 
     it('Test 11: the printStatement static method should log the statement (testing the header) to the console', () => {
         // Arrange
-        const printingSpy = spyOn(global.console, `log`);
+        printingSpy = spyOn(global.console, `log`);
 
         // Act
         Statement.printStatement();
@@ -75,15 +97,31 @@ describe('Testing the Statement class:', () => {
         expect(printingSpy).toHaveBeenCalledWith('date       || credit  || debit  || balance');
     });
 
-    it('Test 12: the printStatement static method should log the deposit details', () => {
+
+    it('Test 12: the printStatement static method should log the details of the deposit', () => {
         // Arrange
-        const printingSpy = spyOn(Statement, `printStatement`);
+        printingSpy = spyOn(Statement, `printStatement`);
+        expected = ['04/07/1990', 300, 0, 300];
 
         // Act
         account1.deposit(MockTransaction1);
         Statement.printStatement(account1.getTransactions());
 
         // Assert
-        expect(printingSpy).toHaveBeenCalledWith([['04/07/1990', 300, 0, 300]]);
+        expect(printingSpy).toHaveBeenCalledWith([expected]);
+    });
+
+
+    it('Test 13: the printStatement static method should log the details of the withdrawal', () => {
+        // Arrange
+        printingSpy = spyOn(Statement, `printStatement`);
+        expected = ['04/07/2023', 0, 50, -50];
+
+        // Act
+        account1.withdraw(MockTransaction2);
+        Statement.printStatement(account1.getTransactions());
+
+        // Assert
+        expect(printingSpy).toHaveBeenCalledWith([expected]);
     });
 });
